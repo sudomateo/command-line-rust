90cc454508e60a22c3d0ed4e74be9cd899bb88ce
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2022-12-03 23:41:44 -0500

    ch02: echor

---
+++ echor/tests/cli.rs
@@ -0,0 +1,53 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["Hello", "there", "-n"], "tests/expected/hello2.n.txt")
+}
+
+#[test]
+fn dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")?
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    Ok(())
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("hello")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("hello"));
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
